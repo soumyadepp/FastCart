3dc67c88c45257785e80cd36b43241aac3de4334
Soumyadeep Ghosh <67363979+soumyadepp@users.noreply.github.com>
2023-02-15 10:37:41 +0530

    Add files via upload

--- src/features/filterSidebar/components/sortFilter/SortFilter.tsx
+++ src/features/filterSidebar/components/sortFilter/SortFilter.tsx
@@ -1,17 +1,20 @@
-import { SortOutlined } from '@mui/icons-material';
-import React from 'react'
+import { ArrowUpward, SortOutlined } from '@mui/icons-material';
 import styles from './SortFilter.module.css';
 export default function SortFilter() {
-  return (
-    <div className={styles.sortFilterWrapper}>
-        <div className={styles.sortFilterHeader}>
-            <div className={styles.sortHeader}>
-                <h3>Sort By</h3>
+    return (
+        <div className={styles.sortFilterWrapper}>
+            <div className={styles.sortFilterHeader}>
+                <div className={styles.sortHeader}>
+                    <h3>Sort By</h3>
+                </div>
+                <div className={styles.sortHeaderIcon}>
+                    <SortOutlined />
+                </div>
             </div>
-            <div className={styles.sortHeaderIcon}>
-            <SortOutlined/>
+            <div className={styles.sortFilterBody}>
+                <button className={styles.sortActionButton}>Price <ArrowUpward color='primary'/></button>
+                <button className={styles.sortActionButton}>Rating <ArrowUpward color='primary'/></button>
             </div>
         </div>
-    </div>
-  )
+    )
 }
